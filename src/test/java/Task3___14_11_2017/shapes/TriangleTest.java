127a5feabf06efdae6c65b8c3f68d6c27461cce2
Victor <vitia0597@gmail.com>
2017-11-23 17:47:18 +0200

    testscorrection

--- src/test/java/Task3___14_11_2017/shapes/TriangleTest.java
+++ src/test/java/Task3___14_11_2017/shapes/TriangleTest.java
@@ -7,26 +7,26 @@ import static org.junit.Assert.*;
 public class TriangleTest {
     @Test
     public void getArea() throws Exception {
-        Triangle triangle = UtilTriangleTest.SIMPLE_TRIANGLE;
-        assertEquals(triangle.getArea(), UtilTriangleTest.AREA, UtilTriangleTest.DELTA);
+        Triangle triangle = new Triangle(new Point(0,0), new Point(0,3), new Point(4,0));
+        assertEquals(triangle.getArea(), 6.0, 0.001);
     }
 
     @Test
     public void isCorrect_WithUncorrectTriangle() throws Exception {
-        Triangle triangle = UtilTriangleTest.INCORRECT_TRIANGLE;
-        assertFalse(triangle.isCorrect());
+        Triangle incorrectTriangle = new Triangle(new Point(0,0), new Point(1,2), new Point(2,4));
+        assertFalse(incorrectTriangle.isCorrect());
     }
 
     @Test
     public void isCorrect_WithCorrectTriangle() throws Exception{
-        Triangle triangle = UtilTriangleTest.SIMPLE_TRIANGLE;
+        Triangle triangle = new Triangle(new Point(0,0), new Point(0,3), new Point(4,0));
         assertTrue(triangle.isCorrect());
     }
 
     @Test
     public void getPerimeter() throws Exception {
-        Triangle triangle = UtilTriangleTest.SIMPLE_TRIANGLE;
-        assertEquals(triangle.getPerimeter(), UtilTriangleTest.PERIMETER, UtilTriangleTest.DELTA);
+        Triangle triangle = new Triangle(new Point(0,0), new Point(0,3), new Point(4,0));
+        assertEquals(triangle.getPerimeter(), 12.0, 0.001);
     }
 
 }
